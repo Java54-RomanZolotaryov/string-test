f1eb1b658389e6cd12e4b21565a18e2b8c5861a6
Roman <romanzolotdev@gmail.com>
2024-03-05 13:04:14 +0200

    init commit

---
+++ src/telran/string/test/StringTest.java
@@ -0,0 +1,19 @@
+package telran.string.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.jupiter.api.Test;
+
+class StringTest {
+
+	@Test
+	void concatTest() {
+		fail();
+	}
+	
+	@Test
+	void containsTest() {
+		fail();
+	}
+
+}
